d19eb8a6ce24a5a29b0f17208c25b891386d3cf5
Ryan <159479854+RyanHobbes@users.noreply.github.com>
2025-05-03 20:24:30 -0500

    Update ToyDataSetMaker.py

--- ToyDataSetMaker.py
+++ ToyDataSetMaker.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-inputCSV1 = r"C:\Users\amesr\Desktop\sqlite-dll-win-x86-3490100\books_data.csv"
-inputCSV2 = r"C:\Users\amesr\Desktop\sqlite-dll-win-x86-3490100\Books_rating.csv"
+inputCSV1 = r"\books_data.csv"  #replace with file path to csvs
+inputCSV2 = r"\Books_rating.csv"
 
-outputCSV1 = r"C:\Users\amesr\Documents\GitHub\BetterReads\first_1000_data.csv"
-outputCSV2 = r"C:\Users\amesr\Documents\GitHub\BetterReads\first_1000_rating.csv"
+outputCSV1 = r"C:\Users\YOURUSERNAMEHERE\Documents\GitHub\BetterReads\first_1000_data.csv"  #if using default path, repalce "YOURUSERNAMEHERE" with username to put in git repo
+outputCSV2 = r"C:\Users\YOURUSERNAMEHERE\Documents\GitHub\BetterReads\first_1000_rating.csv"
 
 df = pd.read_csv(inputCSV1)
 
